530514c0ad6cfe584a9c48a493bd38dd733f70ea
Arseny Kenunen <arsenykenunen@gmail.com>
2026-02-23 15:30:24 +0300

    added empty map element definition

---
+++ objects/mapElement.h
@@ -0,0 +1,14 @@
+//
+// Created by arsen on 23/02/2026.
+//
+
+#ifndef ROVER_MAPELEMENT_H
+#define ROVER_MAPELEMENT_H
+#include "object.h"
+
+
+class MapElement : public Object
+{
+
+};
+#endif //ROVER_MAPELEMENT_H
