7e21730a775e3ad3e87e749c4dec9514d9378101
Arseny Kenunen <arsenykenunen@gmail.com>
2026-03-02 09:07:23 +0300

    added 3 more empty definitions

---
+++ objects/building.h
@@ -0,0 +1,10 @@
+#ifndef ROVER_BUILDING_H
+#define ROVER_BUILDING_H
+#include "block.h"
+
+class Building : public Block
+{
+    public:
+};
+
+#endif //ROVER_BUILDING_H
---
+++ objects/deposit.h
@@ -0,0 +1,10 @@
+#ifndef ROVER_DEPOSIT_H
+#define ROVER_DEPOSIT_H
+#include "block.h"
+
+class Deposit: public Block //месторождение
+{
+    public:
+};
+
+#endif //ROVER_DEPOSIT_H
