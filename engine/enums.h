c2386661a4927a2d9a0001abf8209039a704e699
|GriB28| <gp.off.dev@gmail.com>
2026-03-16 16:57:16 +0300

    глобальные переменные движка

---
+++ engine/enums.h
@@ -0,0 +1,15 @@
+#ifndef ROVER_ENUMS_H
+#define ROVER_ENUMS_H
+
+#include <string>
+using std::string;
+
+namespace game {
+    inline unsigned short DEFAULT_WIDTH  = 1920;
+    inline unsigned short DEFAULT_HEIGHT = 1080;
+    inline string         DEFAULT_TITLE  = "ROVER";
+
+    inline short          SCENES_CAP     = 4;
+}
+
+#endif
