18088fa9a16cfd9b43a88260fe4bfeaf84c621be
|GriB28| <gp.off.dev@gmail.com>
2026-03-23 16:20:36 +0300

    cleared main script

--- main.cpp
+++ main.cpp
@@ -10,36 +10,5 @@
 int main() {
     const game::Engine engine;
     engine.run();
-    unsigned long long frames = 0;
-
-    RenderWindow window(
-        VideoMode({1280, 720}),
-        "SFML-built-test"
-    );
-    window.setFramerateLimit(24);
-
-    Font font;
-    if (!font.openFromFile("fonts/Kuro Regular.otf")) return 1;
-
-    Text frames_text(font);
-    frames_text.setCharacterSize(50);
-    frames_text.setPosition(Vector2f{615, 355});
-
-    Object testObject;
-
-    while (window.isOpen()) {
-        while (const auto event = window.pollEvent())
-            if (event->is<sf::Event::Closed>())
-                window.close();
-
-        window.clear();
-
-        frames_text.setString(to_string(frames));
-        window.draw(frames_text);
-        window.draw(testObject.getSprite());
-
-        window.display();
-        frames++;
-    }
     return 0;
 }
