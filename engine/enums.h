2de093e31710bf262f609c775fe0f1bb93605bf7
|GriB28| <gp.off.dev@gmail.com>
2026-03-30 23:05:02 +0300

    added scenes' enums + linking to a scene counter

--- engine/enums.h
+++ engine/enums.h
@@ -8,13 +8,20 @@ using sf::Time;
 using std::string;
 
 namespace game {
-    inline unsigned short DEFAULT_WIDTH  = 1920;
-    inline unsigned short DEFAULT_HEIGHT = 1080;
+    inline unsigned short DEFAULT_WIDTH  = 1920,
+                          DEFAULT_HEIGHT = 1080;
     inline string         DEFAULT_TITLE  = "ROVER";
 
-    inline short          SCENES_CAP     = 4;
     inline short          UPS            = 30;
     inline Time           UPS_delta      = sf::microseconds(1000000 / UPS);
+
+    namespace scenes {
+        inline unsigned short CAP       = 4,
+                              LOADING   = 0,
+                              MAIN_MENU = 1,
+                              CUTSCENE  = 2,
+                              MAIN_GAME = 3;
+    }
 }
 
 #endif
