573f623c6564149be92a17c2586c3b6e0ee2f3be
Arseny Kenunen <arsenykenunen@gmail.com>
2026-04-05 18:23:43 +0300

    Writing chunk loader in progress

---
+++ objects/map/chunk.cpp
@@ -0,0 +1,71 @@
+#include "chunk.h"
+#include "SFML/Graphics/RenderTarget.hpp"
+
+Chunk::Chunk()
+{
+    constexpr std::array level = {
+        0, 1, 0, 0,
+        1, 2, 2, 1,
+        3, 2, 3, 3,
+        2, 3, 2, 3,
+
+    };
+    load("tileset.png", {32, 32}, level.data(), 4, 4);
+}
+Chunk::~Chunk()
+{
+
+}
+
+void Chunk::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    states.transform *= getTransform(); // getTransform() is defined by sf::Transformable
+    states.texture = &m_tileset;
+    target.draw(vertices, states);
+}
+
+bool Chunk::load(const std::filesystem::path& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+{
+    // load the tileset texture
+    if (!m_tileset.loadFromFile(tileset))
+        return false;
+
+    // resize the vertex array to fit the level size
+    vertices.setPrimitiveType(sf::PrimitiveType::Triangles);
+    vertices.resize(width * height * 6);
+
+    // populate the vertex array, with two triangles per tile
+    for (unsigned int i = 0; i < width; ++i)
+    {
+        for (unsigned int j = 0; j < height; ++j)
+        {
+            // get the current tile number
+            const int tileNumber = tiles[i + j * width];
+
+            // find its position in the tileset texture
+            const int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+            const int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+            // get a pointer to the triangles' vertices of the current tile
+            sf::Vertex* triangles = &vertices[(i + j * width) * 6];
+
+            // define the 6 corners of the two triangles
+            triangles[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+            triangles[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+            triangles[2].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+            triangles[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+            triangles[4].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+            triangles[5].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+
+            // define the 6 matching texture coordinates
+            triangles[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+            triangles[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+            triangles[2].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+            triangles[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+            triangles[4].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+            triangles[5].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+        }
+    }
+
+    return true;
+}
---
+++ objects/map/chunk.h
@@ -0,0 +1,26 @@
+
+#ifndef ROVER_CHUNK_H
+#define ROVER_CHUNK_H
+#include <filesystem>
+
+#include "SFML/Graphics/Drawable.hpp"
+#include "SFML/Graphics/Texture.hpp"
+#include "SFML/Graphics/Transformable.hpp"
+#include "SFML/Graphics/VertexArray.hpp"
+
+class Chunk : public sf::Drawable, public sf::Transformable
+{
+public:
+   Chunk();
+   ~Chunk() override;
+private:
+   void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+   sf::VertexArray vertices;
+   sf::Vector2<int> position;
+   sf::Texture m_tileset;
+
+   bool load(const std::filesystem::path& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height);
+};
+
+
+#endif //ROVER_CHUNK_H
