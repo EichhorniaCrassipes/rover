cccc576a0b5f98f7cbf589de91923fbdd24bbae5
|GriB28| <gp.off.dev@gmail.com>
2026-04-06 15:59:41 +0300

    added player's View and zooming

--- objects/entity/player.cpp
+++ objects/entity/player.cpp
@@ -1,1 +1,11 @@
 #include "player.h"
+
+
+object::Player::Player(View default_view) {
+    current_view = default_view;
+}
+
+
+void object::Player::zoom(const float coefficient) {
+    current_view.zoom(coefficient);
+}
--- objects/entity/player.h
+++ objects/entity/player.h
@@ -3,11 +3,18 @@
 
 #include "mob.h"
 
+#include <SFML/Graphics/View.hpp>
+using sf::View;
+
 namespace object {
     class Player : public Mob {
     public:
-        Player();
+        explicit Player(View default_view);
         ~Player() override = default;
+
+        void zoom(float coefficient);
+    private:
+        View current_view;
     };
 }
 
