b71bf35eff1908fb16016a466a2d4a10a54726a0
|GriB28| <gp.off.dev@gmail.com>
2026-04-14 16:09:49 +0300

    removed camera from player class

--- objects/entity/player.cpp
+++ objects/entity/player.cpp
@@ -7,41 +7,21 @@ using std::exp;
 using std::cout;
 
 
-object::Player::Player(const View &default_view, sf::RenderWindow *window_link) : Mob() {
+object::Player::Player(const Vector2f &null_position) : Mob() {
     cout << "[player/constructor]\n\t"
     << "size: {" << sprite.getScale().x << ';' << sprite.getScale().y << "}\n\t"
     << "global bounds: {" << sprite.getGlobalBounds().size.x << ';' << sprite.getGlobalBounds().size.y << "}\n\t"
     << "position: {" << sprite.getPosition().x << ';' << sprite.getPosition().y << "}\n\n";
 
-    current_view = default_view;
-    position = current_view.getCenter() - sprite.getGlobalBounds().size / 2.f;
+    position = null_position - sprite.getGlobalBounds().size / 2.f;
     setScale({.04, .04});
-    window = window_link;
 }
 
-
-void object::Player::zoom(const float coefficient) {
-    current_view.zoom(coefficient);
-}
 
 void object::Player::move(const Vector2f vector, const float delta_time) {
     cout << "[player/move]\n\t"
     << "moved with vector: {" << vector.x << ';' << vector.y << "}\n\t";
 
-    const auto delta = vector.normalized() * player_speed * delta_time;
+    const auto delta = vector.normalized() * speed * delta_time;
     position += delta;
-
-    move_camera(delta_time);
-}
-
-void object::Player::move_camera(const float delta_time) {
-    const auto distance = position - current_view.getCenter(),
-               delta = exp(-distance.length() * distance_multiplier) * camera_speed * delta_time * distance.normalized();
-
-    cout << "camera move:\n\t\t"
-    << "distance: {" << distance.x << ';' << distance.y << "}\n\t\t"
-    << "delta: {" << delta.x << ';' << delta.y << "}\n\n";
-
-    current_view.move(delta);
-    window->setView(current_view);
 }
--- objects/entity/player.h
+++ objects/entity/player.h
@@ -9,20 +9,12 @@ using sf::View;
 namespace object {
     class Player : public Mob {
     public:
-        explicit Player(const View &default_view, sf::RenderWindow *window_link);
+        explicit Player(const Vector2f &null_position = {0, 0});
         ~Player() override = default;
-
-        void zoom(float coefficient);
 
         void move(Vector2f vector, float delta_time);
     private:
-        void move_camera(float delta_time);
-
-        float player_speed = 250,
-              camera_speed = 200,
-              distance_multiplier = 0.001;
-        View current_view;
-        sf::RenderWindow *window;
+        float speed = 250;
     };
 }
 
