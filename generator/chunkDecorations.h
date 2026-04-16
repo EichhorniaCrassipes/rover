c924f4d8d727f592a52d84f50195b61cab3f902d
|GriB28| <gp.off.dev@gmail.com>
2026-04-16 16:33:16 +0300

    decorations are separated from the chunk

---
+++ generator/chunkDecorations.h
@@ -0,0 +1,59 @@
+#ifndef CHUNK_DECORATIONS
+#define CHUNK_DECORATIONS
+
+#include "mapGenerator.h"
+
+#include <vector>
+using std::vector;
+using std::pair;
+
+#include <string>
+using std::string;
+
+#include "SFML/Graphics/Drawable.hpp"
+using sf::Drawable;
+
+#include "SFML/Graphics/Texture.hpp"
+using sf::Texture;
+
+#include "SFML/Graphics/Transformable.hpp"
+using sf::Transformable;
+
+#include "SFML/Graphics/VertexArray.hpp"
+using sf::VertexArray;
+using sf::RenderStates;
+using sf::RenderTarget;
+
+#include <SFML/System/Vector2.hpp>
+using sf::Vector2u;
+using sf::Vector2i;
+
+
+namespace generator {
+    struct ChunkDecorationsDecorationInfo {
+        string biome;
+        TileDecoration decoration;
+        Vector2f global_coordinates;
+    };
+
+    class ChunkDecorations : public Drawable, public Transformable {
+    public:
+        ChunkDecorations(MapGenerator* generator_link, int x, int y);
+    private:
+        MapGenerator* generator;
+
+        VertexArray decoration_vertices;
+        Vector2i position;
+        Texture m_decorations;
+
+        const sf::Vector2<int> size;
+
+        void draw(RenderTarget &target, RenderStates states) const override;
+        bool load(const string &set_path, Vector2u tileSize);
+
+        vector<ChunkDecorationsDecorationInfo> decorations;
+    };
+}
+
+
+#endif
