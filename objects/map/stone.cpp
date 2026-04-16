b3e801810e03296085562d83b992e5804586ad97
|GriB28| <gp.off.dev@gmail.com>
2026-04-16 13:47:08 +0300

    fixed stone source

--- objects/map/stone.cpp
+++ objects/map/stone.cpp
@@ -1,15 +1,14 @@
 #include "stone.h"
 
 
-Stone::Stone(sf::Vector2f pos,string biome, char variation = 0)
-{
+object::Stone::Stone(const Vector2f pos, const string &biome, const unsigned char variation) {
     position = {pos.x*64,pos.y*64};
-    scale = sf::Vector2f(1,1);
+    scale = Vector2f(1,1);
     if (biome == "test0")
-        texture = sf::Texture("textures/stone.png", false, sf::IntRect({0, variation * 64}, {64, 64}));
+        texture = Texture("textures/stone.png", false, sf::IntRect({0, variation * 64}, {64, 64}));
     if (biome == "test1")
-        texture = sf::Texture("textures/stone.png", false, sf::IntRect({64, variation * 64}, {64, 64}));
-    sprite = sf::Sprite(texture);
+        texture = Texture("textures/stone.png", false, sf::IntRect({64, variation * 64}, {64, 64}));
+    sprite = Sprite(texture);
     sprite.setPosition(position);
     sprite.setScale({1,1});
 }
