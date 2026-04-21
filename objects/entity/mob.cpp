80e0aa7606db7b1f69b65fc2c830d9e675c451e9
|GriB28| <gp.off.dev@gmail.com>
2026-04-21 15:39:12 +0300

    texture applying moved to mob source

--- objects/entity/mob.cpp
+++ objects/entity/mob.cpp
@@ -1,6 +1,17 @@
 #include "mob.h"
 
 
-object::Mob::Mob(const float initial_health, const float speed) : Entity(speed) {
+object::Mob::Mob(
+    const Texture* texture_link,
+    const Vector2i sprite_size,
+    const Vector2f &null_position,
+    const float initial_health,
+    const float speed
+) : Entity(speed)
+{
     health = initial_health;
+
+    texture = *texture_link;
+    sprite.setTextureRect({{0, 0}, sprite_size});
+    position = null_position - static_cast<Vector2f>(sprite_size) / 2.f;
 }
--- objects/entity/mob.h
+++ objects/entity/mob.h
@@ -3,11 +3,20 @@
 
 #include "../entity.h"
 
+#include <SFML/System/Vector2.hpp>
+using sf::Vector2i;
+
 
 namespace object {
     class Mob : public Entity {
     public:
-        explicit Mob(float initial_health, float speed = 100);
+        explicit Mob(
+            const Texture* texture_link,
+            Vector2i sprite_size,
+            const Vector2f &null_position = {0, 0},
+            float initial_health = 1000,
+            float speed = 100
+        );
     protected:
         float health;
         // another properties...
