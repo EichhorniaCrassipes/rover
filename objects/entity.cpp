a4bf1c64977cb7514310e182e05e138f99b3aa38
|GriB28| <gp.off.dev@gmail.com>
2026-04-21 15:39:50 +0300

    move (per frame) moved to entity source

--- objects/entity.cpp
+++ objects/entity.cpp
@@ -10,6 +10,8 @@ float object::Entity::getSpeed() const { return speed; }
 void object::Entity::setSpeed(const float new_speed) { speed = new_speed; }
 
 void object::Entity::move(const Vector2f vector, const float delta_time) {
-    if (!collision)
-        position += vector * speed * delta_time;
+    if (!collision) {
+        const auto delta = vector.normalized() * speed * delta_time;
+        position += delta;
+    }
 }
