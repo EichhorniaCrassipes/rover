02197a2625b7eeebcec3c64026fc509d06733be6
Arseny Kenunen <arsenykenunen@gmail.com>
2026-04-23 22:06:05 +0300

    added 3 biome decorations

--- objects/map/stone.cpp
+++ objects/map/stone.cpp
@@ -8,6 +8,8 @@ object::Stone::Stone(const Vector2f pos, const string &biome, const unsigned cha
         texture = Texture("textures/stone.png", false, sf::IntRect({0, variation * 64}, {64, 64}));
     if (biome == "test1")
         texture = Texture("textures/stone.png", false, sf::IntRect({64, variation * 64}, {64, 64}));
+    if (biome == "test2")
+        texture = Texture("textures/stone.png", false, sf::IntRect({128, variation * 64}, {64, 64}));
     sprite = Sprite(texture);
     sprite.setPosition(position);
     sprite.setScale({1,1});
