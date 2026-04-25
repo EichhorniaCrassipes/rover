9b1cc954a08698589e621191b54b90af2291e424
|GriB28| <gp.off.dev@gmail.com>
2026-04-25 18:05:53 +0300

    new texture library in chunk generation

--- scenes/game/gameScene.h
+++ scenes/game/gameScene.h
@@ -29,7 +29,7 @@ using game::Camera;
 namespace scene {
     class GameScene : public Scene {
     public:
-        explicit GameScene(RenderWindow* window_link, Camera* camera_link, EngineStats* engine_stats_link, map<string, Texture*>* textures);
+        explicit GameScene(RenderWindow* window_link, Camera* camera_link, EngineStats* engine_stats_link);
         ~GameScene() override;
 
         void render() override;
@@ -71,7 +71,6 @@ namespace scene {
               zoom_coefficient = 0.05,
               render_distance = 10,
               render_distance_squared = render_distance * render_distance;
-        map<string, Texture*>* scene_textures;
     };
 }
 
