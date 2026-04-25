33e39538affede0f53fbd61077ce8c77261571c2
|GriB28| <gp.off.dev@gmail.com>
2026-04-25 16:28:05 +0300

    advanced logging in chunk loaders

--- generator/chunk.cpp
+++ generator/chunk.cpp
@@ -36,10 +36,10 @@ generator::Chunk::Chunk(MapGenerator* generator_link, const long long x, const l
             tiles[i + j * size.x] = {texturelist::maptiles[get4tiles(tile0, tile1, tile2, tile3)]};
             var[i + j * size.x] = tile0.variation;
         }
-    cout << "generating: " << timer.restart().asMilliseconds() << " ms\n";
+    cout << "\tgenerating: " << timer.restart().asMilliseconds() << " ms\n";
 
     load({64, 64}, tiles.data(), var.data(), size.x, size.y);
-    cout << "loading: " << timer.getElapsedTime().asMilliseconds() << " ms\n";
+    cout << "\tloading: " << timer.getElapsedTime().asMilliseconds() << " ms\n";
 
     setPosition({static_cast<float>(position.x * size.x * 4), static_cast<float>(position.y * size.y * 4)});
 }
