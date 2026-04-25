9256eaf67fe0cb5d82f81666d09a728cbed797ca
|GriB28| <gp.off.dev@gmail.com>
2026-04-25 19:20:53 +0300

    added inheritance-based temporary code to the unused classes

--- objects/entity/enemy.cpp
+++ objects/entity/enemy.cpp
@@ -1,1 +1,8 @@
 #include "enemy.h"
+
+
+object::Enemy::Enemy(
+    const string &texture_index,
+    const Vector2i &sprite_size,
+    const Vector2f &null_position
+) : Mob(texture_index, sprite_size, null_position) {}
