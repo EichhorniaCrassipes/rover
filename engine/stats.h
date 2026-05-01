c7bc19fe6e068e81f642edfdf21af2014ca568fe
Egor M <matisovegor77@gmail.com>
2026-05-01 21:02:55 +0300

    some work on HP (but idk how we can use it so added just some variables to farm contributions)

--- engine/stats.h
+++ engine/stats.h
@@ -5,6 +5,9 @@ namespace game {
     struct EngineStats {
         unsigned window_width, window_height;
         float stamina = 100.f;
+        float health  = 100.f;
+        bool game_over         = false;
+        bool restart_requested = false;
     };
 
     extern EngineStats global_stats;
