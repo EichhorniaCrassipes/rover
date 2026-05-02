037c8a1850a84ddadb05bddfb270da3f8c28fab5
|GriB28| <gp.off.dev@gmail.com>
2026-05-02 20:19:28 +0300

    some config adjustments

--- generator/config/deposits.cpp
+++ generator/config/deposits.cpp
@@ -3,7 +3,7 @@ using generator::Deposit;
 
 
 array<Deposit, DEPOSITS> generator::GLOBAL_DEPOSITS = {
-    Deposit{"stone", .35, 1, 0, .2, 0, 1},
-    Deposit{"iron", .35, 1, .2, .55, .5, 1},
-    Deposit{"copper", .35, 1, 0, .35, 0, .5}
+    Deposit{"stone", .35, 1, 0, .2, .8, 1},
+    Deposit{"iron", .35, 1, .2, .55, .7, 1},
+    Deposit{"copper", .35, 1, 0, .35, .3, .5}
 };
