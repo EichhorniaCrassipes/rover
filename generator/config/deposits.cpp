f8589abad90f380283190da6d4cd8c6d854c29e8
|GriB28| <gp.off.dev@gmail.com>
2026-05-02 21:09:32 +0300

    final(?) deposit configuration

--- generator/config/deposits.cpp
+++ generator/config/deposits.cpp
@@ -3,8 +3,8 @@ using generator::Deposit;
 
 
 array<Deposit, DEPOSITS> generator::GLOBAL_DEPOSITS = {
-    Deposit{"stone",  .35, .7, 0,  .2,  .95, 1},
-    Deposit{"iron",   .5,  1,  .4, .55, .75, .85},
-    Deposit{"coal",   .35, .7, 0,  .15, .39, .5},
-    Deposit{"copper", .75, 1,  0,  .15, .5,  .61}
+    Deposit{"iron",   .61, .8, .4,  .5,  .75, .85},
+    Deposit{"stone",  .59, .7, .15, .4,  .15, .4},
+    Deposit{"coal",   .35, .5, .7,  1,   .35, .5},
+    Deposit{"copper", .7,  1,  0,   .25, .5,  .71}
 };
